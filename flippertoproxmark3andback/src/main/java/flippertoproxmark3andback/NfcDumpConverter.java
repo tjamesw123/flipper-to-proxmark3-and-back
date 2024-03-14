9b44d45bc4ac22922e334d33ee91306b7427e561
tjamesw123 <94910672+tjamesw123@users.noreply.github.com>
2024-03-14 11:18:47 -0400

    Removed testing statement

--- flippertoproxmark3andback/src/main/java/flippertoproxmark3andback/NfcDumpConverter.java
+++ flippertoproxmark3andback/src/main/java/flippertoproxmark3andback/NfcDumpConverter.java
@@ -34,7 +34,7 @@ public class NfcDumpConverter {
         //args = new String[]{"convert", "Test04u.nfc", "export", "default", "nfc"};
         //args = new String[]{"convert", "Test04u.nfc", "export", "default", "json"};//FlipperZero-04548352951190-dump.json
         //args = new String[]{"convert", "FlipperZero-04548352951190-dump.json", "export", "default", "nfc"};//N203.nfc
-        args = new String[]{"convert", "N203.nfc", "export", "default", "json"};
+        //args = new String[]{"convert", "N203.nfc", "export", "default", "json"};
         //args = new String[]{"convert", "N215.nfc", "export", "default", "json"};
 
         //System.out.println(Constants.flipperFileToCleanedListOfLines(new File("proxmark3-894F163F.nfc")));
